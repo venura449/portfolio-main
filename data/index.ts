bad5ff04aa43bd4189e7215968d058d1b1548c24
J.A Venura Jayaasingha <137485589+venura449@users.noreply.github.com>
2024-11-13 18:36:32 +0530

    first commit

---
+++ data/index.ts
@@ -0,0 +1,213 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Testimonials", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "I prioritize client collaboration, fostering open communication ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Currently building a Online Shopping System",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Airline Ticket Reservation System",
+    des: "The Airline Ticket Reservation project is a web application designed for booking and managing flight tickets. Built with HTML, CSS, and PHP, The front end is crafted with HTML and CSS for a visually appealing and user-friendly experience, while PHP handles backend processes, including flight data retrieval, booking management, and payment processing.",
+    img: "/air.jpg",
+    iconLists: ["/html.svg", "/css.svg", "/js.svg", "/php.svg", "/sql.svg"],
+    link: "#",
+  },
+  {
+    id: 2,
+    title: "Stock Management System",
+    des: "The Inter-Industrial Stock Management System is designed to streamline stock handling and enhance inventory management across various industrial sectors. Built using modern web technologies, this system leverages Java for backend functionality, HTML for structured content, JavaScript and jQuery for interactive elements, and Bootstrap for responsive design.",
+    img: "/stock.jpg",
+    iconLists: ["/html.svg", "/css.svg", "/sql.svg", "/java.svg", "/jsp.svg"],
+    link: "/ui.yoom.com",
+  },
+  {
+    id: 3,
+    title: "Cafe Site Web Project",
+    des: "Developed a fully responsive and user-friendly website for a café, focusing on creating a seamless digital experience that captures the café’s warmth and charm. Key features include interactive menu browsing, online ordering, reservation management, and a gallery showcasing the ambiance. Emphasized an intuitive design, smooth navigation, and functionality to enhance user engagement and reflect the brand's essence.",
+    img: "/food.png",
+    iconLists: ["/html.svg", "/css.svg", "/js.svg", "/php.svg", "/sql.svg"],
+    link: "/ui.aiimg.com",
+  },
+  {
+    id: 4,
+    title: "Green House Controller Module Project",
+    des: "Developed a smart greenhouse project that combines automation with sustainability to optimize plant growth and resource efficiency. Using IoT sensors, the system monitors key environmental factors such as temperature, humidity, soil moisture, and light levels in real-time. Automated controls for irrigation, ventilation, and lighting adjust based on sensor data, creating ideal growing conditions while conserving water and energy. This project provides a scalable, efficient solution for sustainable agriculture, with remote monitoring and control capabilities for improved crop management.",
+    img: "green.jpg",
+    iconLists: ["/ard.svg", "/cad.svg"],
+    link: "/ui.apple.com",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Working with Venura was an outstanding experience. His professionalism, reliability, and commitment to excellence were evident in every phase of our project. Venura’s passion for each detail of the development process truly shines. If you’re looking to take your project to the next level with a dedicated and skilled partner, Venura is the perfect choice.",
+    name: "Sachithma Jayasinghe",
+    title: "CEO of Gravity Edu",
+    image: "/rec1.jpg",
+  },
+
+  {
+    quote:
+      " am incredibly satisfied with the assignment I received! It was meticulously done, thoroughly researched, and exactly met the requirements. The quality of work was impressive, with clear explanations and well-organized content that made everything easy to understand. Communication was smooth, and the assignment was delivered on time. Highly recommend for anyone looking for reliable, top-notch assistance!",
+    name: "Anonymous",
+    title: "Student @ SLIIT",
+    image: "/img.png",
+  },
+
+  {
+    quote:
+      "I am extremely pleased with the Online Shopping Management System! It’s intuitive, efficient, and has streamlined our entire shopping process. Inventory management, order tracking, and customer service are all seamlessly integrated, making it easy to oversee operations and deliver excellent service. The system is user-friendly, reliable, and has significantly improved our productivity. Highly recommended for businesses looking to enhance their online shopping experience!",
+    name: "Anonymous",
+    title: "Student @ ICBT",
+    image: "/img.png",
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "cloudinary",
+    img: "/cloud.svg",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 2,
+    name: "appwrite",
+    img: "/app.svg",
+    nameImg: "/appName.svg",
+  },
+  {
+    id: 3,
+    name: "HOSTINGER",
+    img: "/host.svg",
+    nameImg: "/hostName.svg",
+  },
+  {
+    id: 4,
+    name: "stream",
+    img: "/s.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 5,
+    name: "docker.",
+    img: "/dock.svg",
+    nameImg: "/dockerName.svg",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Worked As Arduino Instructor",
+    desc: "Worked as Arduino instructor @ Mahinda Rajapaksha College on 2k18 MRORC(Mahinda Rajapaksha College Robotic And Innovaters Club)",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "FullStack Developer",
+    desc: "Designed and developed Web interface including Front end And Back End @ Gravity Industries",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp2.svg",
+  },
+  {
+    id: 3,
+    title: "Freelance App/Web Project Developer",
+    desc: "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp3.svg",
+  },
+  {
+    id: 4,
+    title: "IoT System Developer",
+    desc: "Developed and maintained creative solutions for day today problems,using Internet Of Things",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    link: "https://github.com/venura449",
+  },
+  {
+    id: 2,
+    img: "insta.png",
+    link: "https://www.instagram.com/venu_nasa/profilecard/?igsh=MXJ1eWxtNm1wNnF3eA==",
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+    link: "www.linkedin.com/in/venura123",
+  },
+];
